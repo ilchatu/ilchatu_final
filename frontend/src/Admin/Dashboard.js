ff905e571cb01437c1292c9bf57e89e5fb3c3531
ilchatu <152178314+ilchatu@users.noreply.github.com>
2024-02-11 17:49:08 +0800

    Update Dashboard.js

--- frontend/src/Admin/Dashboard.js
+++ frontend/src/Admin/Dashboard.js
@@ -13,7 +13,7 @@ const Dashboard = () => {
   useEffect(() => {
     const fetchDocumentCount = async (collectionName, setStateFunction) => {
       try {
-        const response = await fetch(`http://localhost:5000/api/count/${collectionName}`);
+        const response = await fetch(`https://ilcha.onrender.com/api/count/${collectionName}`);
         const data = await response.json();
         setStateFunction(data.count);
       } catch (error) {
