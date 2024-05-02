604bae249c9f028a360100eed42cad5da11494af
ilchatu <152178314+ilchatu@users.noreply.github.com>
2024-05-02 22:34:28 +0800

    Update reportController.js

--- backend/controllers/reportController.js
+++ backend/controllers/reportController.js
@@ -1,10 +1,12 @@
 const ReportMod = require("../models/reportModel");
 const User = require("../models/userModel");
+const Message = require("../models/reportModel");
 
 const createReport = async (req, res) => {
   const report = new ReportMod(req.body);
   try {
     const newreport = await report.save();
+      console.log("Logs",report);
     res.status(201).json(newreport);
   } catch (error) {
     res.status(400).json({ message: error.message });
@@ -13,12 +15,16 @@ const createReport = async (req, res) => {
 
 const allReport = async (req, res) => {
   try {
-    const report = await ReportMod.find().populate({
-      path: "user",
-      model: User,
-    });
-    res.json(report);
+    const reports = await ReportMod.find()
+      .populate({
+        path: "user",
+        model: User,
+      });
+    
+    
+    res.json(reports);
   } catch (error) {
+    console.error("Error fetching reports:", error); // Log any errors
     res.status(500).json({ message: error.message });
   }
 };
