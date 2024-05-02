a3c9f9ad8a7917d3483b75dc064db4b13e1c5865
ilchatu <152178314+ilchatu@users.noreply.github.com>
2024-05-02 22:36:06 +0800

    Update messageRoutes.js

--- backend/routes/messageRoutes.js
+++ backend/routes/messageRoutes.js
@@ -1,12 +1,13 @@
 const express = require("express");
-const { sendMessage, allMessages } = require("../controllers/messageController");
+const { sendMessage, allMessages, deleteMessage, markAsDeleted } = require("../controllers/messageController");
 const { protect } = require("../middleware/authMiddleware");
-const { deleteMessage } = require("../controllers/messageController");
 
 const router = express.Router();
 
-router.route("/").post(protect, sendMessage);       
+router.route("/").post(protect, sendMessage);
 router.route("/:chatId").get(protect, allMessages);
+router.route("/:messageId").delete(protect, deleteMessage); // Endpoint for deleting a message
+router.put("/:messageId/mark-as-deleted", markAsDeleted);
 router.route("/delete/:messageId").delete(protect, deleteMessage);
 
 module.exports = router;
